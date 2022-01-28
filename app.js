7e85a2fcc87faa431d5a542d8536bb61bd3b0549
danalvrz <dante.alvrz@gmail.com>
2022-01-28 08:35:29 -0600

    Add solution

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-// Write a program in fizzbuzz.js that prints an array of integers from 1 to 100.
-// For multiples of 3, show "Fizz" instead of the number.
-// For multiples of 5, show "Buzz" instead of the number.
-// For multiples of 3 and 5, show "FizzBuzz" instead of the number.
-// Return an array containing the results based on the rules above.
-
-const test = () => {
-    for (let i = 1; i < 101; i += 1) {
-        if (i % 3 === 0 && i % 5 === 0) { 
-            console.log('FizzBuzz');
-        } else if (i % 5 === 0) {
-            console.log('Buzz');
-        } else if (i % 3 === 0){
-            console.log('Fizz');
+function gradingStudents(grades) {
+    // Write your code here
+    const arr = [];
+    for (let i =0; i < grades.length; i++) {
+        const otherNumber = Math.ceil(grades[i] / 5)*5;
+        if (otherNumber - grades[i] < 3 && grades[i] > 37){
+            arr.push(otherNumber);
         } else {
-            console.log(i);
+            arr.push(grades[i]);
         }
-        
     }
+console.log(arr);
+return arr
 }
 
-test();
-// this worked!
+//this worked
